4e9b5bd68e5ca4e73d904792e16e715fccf861d6
xzh <>
2021-06-18 11:05:36 +0800

    服务器配置

--- Sources/App/Application/configure.swift
+++ Sources/App/Application/configure.swift
@@ -41,6 +41,36 @@ public func configure(_ app: Application) throws {
     default: break
         
     }
+    app.logger.info("app ---- 配置默认server")
+    
+    //配置主机名 默认127.0.0.1
+    app.http.server.configuration.hostname = "127.0.0.1"
+    //配置端口号
+    app.http.server.configuration.port = 8083
+    //处理队列的最大长度 默认256
+    app.http.server.configuration.backlog = 128
+    //复用地址
+    app.http.server.configuration.reuseAddress = false
+    //TCP无延迟
+    app.http.server.configuration.tcpNoDelay = true
+    //响应压缩 使用 gzip 控制 HTTP 响应压缩
+    app.http.server.configuration.responseCompression = .enabled
+    //请求解压缩 使用 gzip 控制 HTTP 请求解压缩
+    app.http.server.configuration.requestDecompression = .enabled
+    //通道 启用对 HTTP 请求和响应流水线的支持
+    app.http.server.configuration.supportPipelining = true
+    //支持版本 控制服务器将使用哪些HTTP版本。默认情况下，启用TLS时，Vapor将同时支持HTTP/1和HTTP/2。禁用TLS时，仅支持HTTP/1
+    app.http.server.configuration.supportVersions = [.one]
+    //名称
+    app.http.server.configuration.serverName = "artserver"
+    //TLS
+    //手动启动
+    try app.server.start()
+    //请求关闭
+    app.server.shutdown()
+    
+    
+    
     ArtLogger.artLogger().warning("自定义日志", metadata: nil)
     app.logger.info("app ****** 配置完成")
 }
